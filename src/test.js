6c9a6c715fc5f77370a3ddf0eb1983fceadd3795
Alexon1999 <alexon1999@gmail.com>
2020-07-21 20:21:56 +0200

    klhjkn

--- src/test.js
+++ src/test.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const test = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt fuga
+        culpa esse quam nihil repellendus error modi deleniti reprehenderit
+        explicabo nemo repudiandae, voluptatem eum aliquid? Deleniti, ut
+        excepturi, nam similique quod ea voluptas quisquam hic fuga vitae eius?
+        Accusantium, reiciendis placeat! Consequatur, itaque sapiente quae alias
+        porro odio ut exercitationem.
+      </p>
+    </div>
+  );
 };
 
 export default test;
